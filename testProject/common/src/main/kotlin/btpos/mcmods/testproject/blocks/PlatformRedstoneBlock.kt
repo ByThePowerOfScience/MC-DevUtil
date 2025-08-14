8db92fea2bac295aec4aae0514a0d46f23d6b713
ByThePowerOfScience <16433721+ByThePowerOfScience@users.noreply.github.com>
2025-08-14 17:37:46 -0400

    Commit testproject

---
+++ testProject/common/src/main/kotlin/btpos/mcmods/testproject/blocks/PlatformRedstoneBlock.kt
@@ -0,0 +1,6 @@
+package btpos.mcmods.testproject.blocks
+
+import net.minecraft.world.level.block.Block
+
+class PlatformRedstoneBlock(props: Properties) : Block(props) {
+}
---
+++ testProject/common/src/main/kotlin/btpos/mcmods/testproject/registry/ModBlocks.kt
@@ -0,0 +1,11 @@
+package btpos.mcmods.testproject.registry
+
+import net.minecraft.world.level.block.Block
+import btpos.mcmods.devutil.common.registry.IObjectRegistry
+
+object ModBlocks : IObjectRegistry {
+
+	override fun register() {
+		
+	}
+}
